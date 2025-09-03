6ae9766a15a21c36f500566dbbcc4f47f4b91890
Christophorus Dryantoro <christophorusdryantoro@gmail.com>
2025-09-03 19:04:59 +0800

    update login placeholder

--- package/components/auth/LoginForm.vue
+++ package/components/auth/LoginForm.vue
@@ -28,7 +28,7 @@ function handleLogin() {
         variant="outlined"
         hide-details
         color="primary"
-        placeholder="Enter your Telegram ID"
+        placeholder="Enter your NIK"
       />
     </v-col>
     <v-col cols="12">
